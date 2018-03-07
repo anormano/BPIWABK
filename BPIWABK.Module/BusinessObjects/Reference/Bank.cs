127d71a325a783d0af20bce1698c2ddd195f417c
Norman PC <normano@live.com>
2018-03-07 12:45:34 +0700

    ok

--- BPIWABK.Module/BusinessObjects/Reference/Bank.cs
+++ BPIWABK.Module/BusinessObjects/Reference/Bank.cs
@@ -17,7 +17,7 @@ namespace BPIWABK.Module.BusinessObjects.Reference
 {
     [DefaultClassOptions]
     //[ImageName("BO_Contact")]
-    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    [DefaultProperty("Bank")]
     //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
     //[Persistent("DatabaseTableName")]
     // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
